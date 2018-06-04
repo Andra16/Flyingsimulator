8150d0f3949f85509f59ea78615309135b4bcb53
Noviani Litriani <nvianiltr@gmail.com>
2018-06-04 23:05:54 +0700

    add start page

--- Final Project/js/initScene.js
+++ Final Project/js/initScene.js
@@ -1,94 +1,92 @@
 var canvas, engine, scene, camera, airplane;
 document.addEventListener('DOMContentLoaded', function () {
-    //get canvas
     canvas = document.getElementById('renderCanvas');
 
-    //create babylon engine
     engine = new BABYLON.Engine(canvas, true);
 
-    var createScene = function () {
-        //create scene
-        scene = new BABYLON.Scene(engine); //play scene
+    scene = createScene();
+});
 
-        scene.clearColor = new BABYLON.Color3(0.2, 0.5, 0.9);
-        scene.ambientColor = new BABYLON.Color3(0.3, 0.3, 0.3);
+function createScene() {
+    //create scene
+    scene = new BABYLON.Scene(engine); //play scene
 
-        scene.fogMode = BABYLON.Scene.FOGMODE_EXP2;
-        scene.fogDensity = 0.002;
-        scene.fogColor = new BABYLON.Color3(0.9, 0.9, 0.9);
+    scene.clearColor = new BABYLON.Color3(0.2, 0.5, 0.9);
+    scene.ambientColor = new BABYLON.Color3(0.3, 0.3, 0.3);
 
-        // Skybox
-        var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
-        var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
-        skyboxMaterial.backFaceCulling = false;
-        skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("TropicalSunnyDay/TropicalSunnyDay", scene);
-        skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-        skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
-        skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-        skybox.material = skyboxMaterial;
+    scene.fogMode = BABYLON.Scene.FOGMODE_EXP2;
+    scene.fogDensity = 0.002;
+    scene.fogColor = new BABYLON.Color3(0.9, 0.9, 0.9);
 
-        // Parameters: name, position, scene
-        var camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 10, -10), scene);
+    // Skybox
+    var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("TropicalSunnyDay/TropicalSunnyDay", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
 
-        //The goal distance of camera from target
-        camera.radius = 30;
+    // Parameters: name, position, scene
+    var camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 10, -10), scene);
 
-        // The goal height of camera above local origin (centre) of target
-        camera.heightOffset = 20;
+    //The goal distance of camera from target
+    camera.radius = 30;
 
-        // The goal rotation of camera around local origin (centre) of target in x y plane
-        camera.rotationOffset = 0;
+    // The goal height of camera above local origin (centre) of target
+    camera.heightOffset = 10;
 
-        //Acceleration of camera in moving from current to goal position
-        camera.cameraAcceleration = 0.005
+    // The goal rotation of camera around local origin (centre) of target in x y plane
+    camera.rotationOffset = 0;
 
-        //The speed at which acceleration is halted
-        camera.maxCameraSpeed = 10
+    //Acceleration of camera in moving from current to goal position
+    camera.cameraAcceleration = 0.005;
 
-        // This attaches the camera to the canvas
-        camera.attachControl(canvas, true);
+    //The speed at which acceleration is halted
+    camera.maxCameraSpeed = 10;
 
-        // NOTE:: SET CAMERA TARGET AFTER THE TARGET'S CREATION AND NOTE CHANGE FROM BABYLONJS V 2.5
-        //targetMesh created here
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
 
-        // create a basic light, aiming 0,8,0
-        var light = new BABYLON.HemisphericLight('hlight', new BABYLON.Vector3(0, 8, 0), scene);
+    // NOTE:: SET CAMERA TARGET AFTER THE TARGET'S CREATION AND NOTE CHANGE FROM BABYLONJS V 2.5
+    //targetMesh created here
 
-        var land = BABYLON.Mesh.CreateGroundFromHeightMap(
-            'island',
-            'island_heightmap.png',
-            100, // width of the ground mesh (x axis)
-            100, // depth of the ground mesh (z axis)
-            40,  // number of subdivisions
-            0,   // min height
-            50,  // max height
-            scene
-        );
-        land.position = new BABYLON.Vector3(0, 0, 0);
+    // create a basic light, aiming 0,8,0
+    var light = new BABYLON.HemisphericLight('hlight', new BABYLON.Vector3(0, 8, 0), scene);
 
-        BABYLON.SceneLoader.ImportMesh("", "", "airplane1.babylon", scene, function (newMeshes) {
-            airplane = newMeshes[0];
-            setup(airplane);
-            camera.lockedTarget = airplane; //version 2.5 onwards
+    var land = BABYLON.Mesh.CreateGroundFromHeightMap(
+        'island',
+        'island_heightmap.png',
+        100, // width of the ground mesh (x axis)
+        100, // depth of the ground mesh (z axis)
+        40,  // number of subdivisions
+        0,   // min height
+        50,  // max height
+        scene
+    );
+    land.position = new BABYLON.Vector3(0, 0, 0);
 
-        });
+    BABYLON.SceneLoader.ImportMesh("", "", "airplane1.babylon", scene, function (newMeshes) {
+        airplane = newMeshes[0];
+        setup(airplane);
+        camera.lockedTarget = airplane; //version 2.5 onwards
 
-        // simple wireframe material
-        var material = new BABYLON.StandardMaterial('material', scene);
-        material.wireframe = true;
-        land.material = material;
+    });
 
+    // simple wireframe material
+    var material = new BABYLON.StandardMaterial('material', scene);
+    material.wireframe = true;
+    land.material = material;
 
-        return scene;
-    }
+    return scene;
+};
 
-    var scene = createScene();
-
+function _render(){
     engine.runRenderLoop(function () {
         scene.render();
     });
-});
-
+}
 
 function setup(mesh) {
     meshX = 0;
